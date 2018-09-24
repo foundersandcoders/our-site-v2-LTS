934b9596d7ac11b369816d08f1ca87a84f881b1f
maxgerber <mgerber94@gmail.com>
2018-09-24 11:30:58 +0100

    + Add Tachyons and PostCSS to project
    Relates #10

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,18 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require('postcss-import')(), // allows you to use @import
+          require('autoprefixer')(), // adds vendor prefixes like -webkit or -moz to your css for better support
+          require('postcss-custom-media')(), // can set media queries as variables
+          require('postcss-custom-properties')(), // allows you to use css variables e.g. var(--my-variable)
+          require('postcss-clean')(), // minifies the output css (i.e. removes all the spaces and comments)
+        ],
+      },
+    },
     'gatsby-plugin-offline',
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
+import '../styles/index.css'
+
 import Header from './header'
 import './layout.css'
 
@@ -32,9 +34,7 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: '0 auto',
-            maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
           }}
         >
           {children}
