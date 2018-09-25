c65faa5d4bcf650698def3acde10eec0d679fc15
maxgerber <mgerber94@gmail.com>
2018-09-25 12:16:30 +0100

    Create Topnav Component
    Relates #11

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-const IndexPage = () => (
+const StoriesPage = () => (
   <Layout>
     <main />
   </Layout>
 )
 
-export default IndexPage
+export default StoriesPage
