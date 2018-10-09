9cc42669e437c9a30cb5ffa4aa1df0ecfb592cd7
Ivan <ivangonzalez@rocketmail.com>
2018-10-09 12:53:54 +0100

    Readd layout

---
+++ src/components/Layout.js
@@ -0,0 +1,54 @@
+import React from "react"
+import styled from "styled-components"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { StaticQuery, graphql } from "gatsby"
+
+import "../styles/index.css"
+
+import Header from "./Header"
+import Menu from "./Menu"
+
+const AppWrapper = styled.div.attrs({
+  className: "center",
+})`
+  max-width: 1280px;
+`
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <AppWrapper>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <Menu />
+          <div>{children}</div>
+        </AppWrapper>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
