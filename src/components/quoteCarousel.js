cc410a88af0903039923530a0645c2444a3c1779
Ivan <ivangonzalez@rocketmail.com>
2018-10-10 13:09:07 +0100

    Style partner quotes #31

--- src/components/quoteCarousel.js
+++ src/components/quoteCarousel.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import quotemarks from "../assets/icons/quotemarks.svg"
 
@@ -13,72 +13,59 @@ const Text = styled.p.attrs({
   width: fit-content;
 `
 
-const TextContainer = styled.div.attrs({
-  className: "flex items-end flex-column",
-})``
-
-const NameAndCompany = ({ firstName, secondName, company }) => (
-  <TextContainer>
-    <Text font="ttu fw5 f-subheadline lh-1">{firstName}</Text>
-    <Text font="ttu fw5 f-subheadline lh-1">{secondName}</Text>
+const NameAndCompany = ({ firstName, secondName, company, className }) => (
+  <div className={className}>
+    <Text font="ttu fw5 font-1">{firstName}</Text>
+    <Text font="ttu fw5 font-1">{secondName}</Text>
     <Text font="font-5">{company}</Text>
-  </TextContainer>
-)
-
-const QuoteSlideContainer = styled.div.attrs({
-  className: "bg-white mv6 ph3 pv2",
-})``
-
-const QuoteImage = styled.img.attrs({
-  className: "ml1",
-})``
-
-const QuoteSlide = ({ quote, firstName, secondName, company }) => (
-  <div className="sliderBoxes">
-    <QuoteImage src={quotemarks} />
-    <QuoteSlideContainer quote={quote}>
-      <NameAndCompany
-        firstName={firstName}
-        secondName={secondName}
-        company={company}
-      />
-      {quote}
-    </QuoteSlideContainer>
   </div>
 )
 
-// class QuoteCarousel extends React.Component {
-//   render() {
-//     return (
-//       <QuoteCarouselContainer>
-//         <QuoteSlide
-//           quote="This is my quote"
-//           firstName="Lucy"
-//           secondName="King"
-//           company="InFact"
-//         />
-//       </QuoteCarouselContainer>
-//     )
-//   }
-// }
+const _NameAndCompany = styled(NameAndCompany).attrs({
+  className: "absolute flex items-end flex-column",
+})`
+  bottom: -72px;
+  right: -166px;
+`
+
+const QuoteSlideContainer = styled.div.attrs({
+  className: "bg-white mt6 ph3 pv2 tc font-3 i",
+})`
+  margin-bottom: 152px;
+`
+
+const QuoteImage = styled.div.attrs({
+  className: "absolute h2",
+})`
+  width: 39px;
+  top: -16px;
+  left: 5px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  ${({ src }) =>
+    css`
+      background-image: url(${src});
+    `};
+`
+
+const QuoteSlide = ({ quote, firstName, secondName, company }) => (
+  <div className="w-33 relative center bg-white">
+    <QuoteImage src={quotemarks} />
+    <QuoteSlideContainer>{quote}</QuoteSlideContainer>
+    <_NameAndCompany
+      firstName={firstName}
+      secondName={secondName}
+      company={company}
+    />
+  </div>
+)
 
 class QuoteCarousel extends React.Component {
   render() {
     return (
       <QuoteCarouselContainer>
         <div>
-          <QuoteSlide
-            quote="You understand our business and which developers will work best in our environment. I love that you offer diverse developers from all backgrounds and cultures. The developers we’ve hired have seamlessly slotted into our team here at Unruly."
-            firstName="Vinny"
-            secondName="Marino"
-            company="Unruly"
-          />
-          <QuoteSlide
-            quote="You understand our business and which developers will work best in our environment. I love that you offer diverse developers from all backgrounds and cultures. The developers we’ve hired have seamlessly slotted into our team here at Unruly."
-            firstName="Vinny"
-            secondName="Marino"
-            company="Unruly"
-          />
           <QuoteSlide
             quote="You understand our business and which developers will work best in our environment. I love that you offer diverse developers from all backgrounds and cultures. The developers we’ve hired have seamlessly slotted into our team here at Unruly."
             firstName="Vinny"
