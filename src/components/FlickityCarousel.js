5bc0fa2353592163621ac0a2183225e1dc2e3a44
Ivan <ivangonzalez@rocketmail.com>
2018-10-10 16:12:59 +0100

    Import flickity after checking window element #31

--- src/components/FlickityCarousel.js
+++ src/components/FlickityCarousel.js
@@ -1,17 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
-import Flickity from "react-flickity-component"
 
-const _Flickity = styled(Flickity).attrs({
-  className: "outline-0 w-100 mb7",
-})`
-  overflow: hidden;
-`
+class InfiniteCarousel extends Component {
+  state = { Flickity: null }
+  constructor(props) {
+    super(props)
+    if (typeof window !== "undefined") {
+      this.state.Flickity = require("react-flickity-component")
+    }
+  }
 
-const InfiniteCarousel = ({ children, options }) => (
-  <_Flickity options={options} disableImagesLoaded={true}>
-    {children}
-  </_Flickity>
-)
+  render() {
+    const { Flickity } = this.state
+
+    if (!Flickity) return <div />
+
+    const { children, options } = this.props
+
+    const _Flickity = styled(Flickity).attrs({
+      className: "outline-0 w-100 mb7",
+    })`
+      overflow: hidden;
+    `
+
+    return (
+      <_Flickity options={options} disableImagesLoaded={true}>
+        {children}
+      </_Flickity>
+    )
+  }
+}
 
 export default InfiniteCarousel
