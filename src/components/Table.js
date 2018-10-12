3df437e9307b7d002dcc134578ab43d2f5641bb4
Ivan <ivangonzalez@rocketmail.com>
2018-10-12 15:41:29 +0100

    Adjust table spacing

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,7 @@ const _Table = styled.table.attrs({
 })``
 
 const _HeaderCell = styled.th`
-  width: 156px;
+  width: 20%;
 `
 
 const TopBorder = styled.div.attrs({
@@ -15,7 +15,15 @@ const TopBorder = styled.div.attrs({
 
 const _Cell = styled.td.attrs({
   className: "pv5 mh2",
-})``
+})`
+  width: 20%;
+`
+
+const Cell = ({ children }) => (
+  <_Cell>
+    <p className="w-80">{children}</p>
+  </_Cell>
+)
 
 const _Row = styled.tr.attrs({
   className: "bb b--black-20 bw1",
@@ -44,25 +52,25 @@ const Table = () => (
     </thead>
     <tbody>
       <_Row>
-        <_Cell>Spring 2019</_Cell>
-        <_Cell>Until 30th November</_Cell>
-        <_Cell>1st December - 31st December</_Cell>
-        <_Cell>7th January - 12th January</_Cell>
-        <_Cell>4th March, 2019 - June 21st, 2019</_Cell>
+        <Cell>Spring 2019</Cell>
+        <Cell>Until 30th November</Cell>
+        <Cell>1st December - 31st December</Cell>
+        <Cell>7th January - 12th January</Cell>
+        <Cell>4th March, 2019 - June 21st, 2019</Cell>
       </_Row>
       <_Row>
-        <_Cell>Summer 2019</_Cell>
-        <_Cell>January - March</_Cell>
-        <_Cell>1st April - 30th April</_Cell>
-        <_Cell>7th May - 11th May</_Cell>
-        <_Cell>1st July 2019 - 18th October, 2019</_Cell>
+        <Cell>Summer 2019</Cell>
+        <Cell>January - March</Cell>
+        <Cell>1st April - 30th April</Cell>
+        <Cell>7th May - 11th May</Cell>
+        <Cell>1st July 2019 - 18th October, 2019</Cell>
       </_Row>
       <_Row>
-        <_Cell>Winter 2019</_Cell>
-        <_Cell>May - July</_Cell>
-        <_Cell>1st August - 31st August</_Cell>
-        <_Cell>2nd September - 7th September</_Cell>
-        <_Cell>28th October 2019 - 21st February 2020</_Cell>
+        <Cell>Winter 2019</Cell>
+        <Cell>May - July</Cell>
+        <Cell>1st August - 31st August</Cell>
+        <Cell>2nd September - 7th September</Cell>
+        <Cell>28th October 2019 - 21st February 2020</Cell>
       </_Row>
     </tbody>
     <tfoot>
