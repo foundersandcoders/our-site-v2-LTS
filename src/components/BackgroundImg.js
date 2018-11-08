5724d40dc52ea6076140683d9f52689f9316b821
Jessie Beech <jessie@jessiebeech.com>
2018-11-08 11:54:24 +0000

    fix for bgimgs without gradients Relates #63

--- src/components/BackgroundImg.js
+++ src/components/BackgroundImg.js
@@ -3,7 +3,11 @@ import styled, { css } from "styled-components"
 const BackgroundImg = styled.div`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
-  background: ${({ src, gradient }) => `${gradient}, url(${src}) no-repeat center`};
+  background: ${ 
+    ({ src, gradient }) => gradient 
+    ? `${gradient} , url(${src}) no-repeat center`
+    : `url(${src}) no-repeat center` }
+  };
   background-size: ${({ bgSize = "cover" }) => bgSize};
 `
 
