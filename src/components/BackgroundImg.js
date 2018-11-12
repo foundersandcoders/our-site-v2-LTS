1ad4b3b322954a2c5694491aedc98c372b5c80ea
Max Gerber <mgerber94@gmail.com>
2018-11-12 16:06:47 +0000

    Update src/components/BackgroundImg.js
    Co-Authored-By: developess <31890915+developess@users.noreply.github.com>

--- src/components/BackgroundImg.js
+++ src/components/BackgroundImg.js
@@ -4,7 +4,7 @@ const BackgroundImg = styled.div`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   background: ${ 
-    ({ src, gradient }) => gradient 
+`${ gradient + ' ,'  || '' } url(${src}) no-repeat center`
   background-size: ${({ bgSize = "cover" }) => bgSize};
 `
 
