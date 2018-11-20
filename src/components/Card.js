e73b729f30a0ab6a01e460d9106b925797ca8b25
Jessie Beech <jessie@jessiebeech.com>
2018-11-20 17:34:45 +0000

    add first blue images and others ready to go Relates #50 #57

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,16 @@ import { breakpoint } from "../styles/utils"
 import dan from "../assets/portraits/dan.png"
 import ines from "../assets/portraits/ines.png"
 import bez from "../assets/portraits/bez.png"
+import amelieBlue from "../assets/portraits/amelie_blue.png"
+import ameliePlain from "../assets/portraits/amelie_plain.png"
+import fatimatBlue from "../assets/portraits/fatimat_blue.png"
+import fatimatPlain from "../assets/portraits/fatimat_plain.png"
+
 
 const _Photo = styled.div.attrs({
   className: "db absolute-ns top-0 ba bw3 b--yellow",
 })`
+  transition: 0.75s ease;
   background: ${({blueImg}) => `url(${blueImg})`};
   &:hover {
     background: ${({normalImg}) => `url(${normalImg})`};
@@ -130,14 +136,15 @@ const Card = ({
   secondName,
   thirdName,
   title,
-  img,
+  normalImg,
+  blueImg,
   imgHeight,
   cardWidth,
   cardHeight,
   quote,
 }) => (
   <Container cardWidth={cardWidth} cardHeight={cardHeight}>
-    <_Photo normalImg={img} blueImg={img} imgHeight={imgHeight} />
+    <_Photo normalImg={normalImg} blueImg={blueImg} imgHeight={imgHeight} />
     <Title
       imgHeight={imgHeight}
       firstName={firstName}
@@ -155,14 +162,15 @@ const StaffCard = ({
   thirdName,
   title,
   title2,
-  img,
+  blueImg,
+  normalImg,
   imgHeight,
   cardWidth,
   cardHeight,
   quote,
 }) => (
   <Container cardWidth={cardWidth} cardHeight={cardHeight}>
-    <_StaffPhoto normalImg={img} blueImg={img} imgHeight={imgHeight} />
+    <_StaffPhoto normalImg={normalImg} blueImg={blueImg} imgHeight={imgHeight} />
     <StaffTitle
       imgHeight={imgHeight}
       firstName={firstName}
@@ -184,7 +192,8 @@ const Dan = () => (
     cardWidth={{ l: "33%", m: "45%", s: "66%" }}
     cardHeight={{ ns: "50%", m: "30%", s: "17%" }}
     imgHeight="75%"
-    img={dan}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
   />
 )
 const Ines = () => (
@@ -195,7 +204,8 @@ const Ines = () => (
     cardWidth={{ ns: "33%", m: "45%", s: "66%" }}
     cardHeight={{ ns: "50%",m: "30%", s: "17%" }}
     imgHeight="75%"
-    img={ines}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
   />
 )
 
@@ -208,7 +218,8 @@ const Yvonne = () => (
     cardWidth={{ ns: "33%", m: "45%", s: "66%" }}
     cardHeight={{ ns: "50%",m: "30%", s: "17%" }}
     imgHeight="75%"
-    img={ines}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
   />
 )
 
@@ -220,7 +231,8 @@ const Joe = () => (
     cardWidth={{ ns: "33%", m: "45%", s: "66%" }}
     cardHeight={{ ns: "50%",m: "30%", s: "17%" }}
     imgHeight="75%"
-    img={ines}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
   />
 )
 
@@ -232,7 +244,8 @@ const Rebecca = () => (
     cardWidth={{ ns: "33%", m: "45%", s: "66%" }}
     cardHeight={{ ns: "50%", m: "30%", s: "17%" }}
     imgHeight="75%"
-    img={ines}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
   />
 )
 
@@ -241,7 +254,8 @@ const Bez = () => (
     firstName="Besart"
     secondName="Hoxhaj"
     title="FAC cohort #2"
-    img={bez}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“The most valuable thing I’ve taken away from the course is the community. 100%. On a personal level it helped me find a real sense of purpose when I needed one, brought on by the amount of people that have worked so hard and how much energy has been invested in it to keep it going.”"
@@ -254,6 +268,8 @@ const Amelie = () => (
     secondName="Chan"
     title="FAC cohort #11"
     img={bez}
+    blueImg={amelieBlue}
+    normalImg={ameliePlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“Because Founders and Coders is founded on the idea of peer-led learning, it stimulates an environment of support and mutual growth, which results in what feels like a very special community: a safe, creative space, where no question is too silly to ask and no idea is too bold to suggest.”"
@@ -264,7 +280,8 @@ const Helen = () => (
     firstName="Helen"
     secondName="Zhou"
     title="FAC cohort #13"
-    img={bez}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“Founders and Coders gave me the confidence to, given time and perseverance, grasp complex concepts which initially seemed daunting. I learned how to walk through code thoroughly and truly understand it in order to solve bugs and errors. And I discovered the joys of paired programming - that learning or tackling a problem together can be one of the most rewarding experiences. ”"
@@ -276,7 +293,8 @@ const Owen = () => (
     secondName="Turner-"
     thirdName="Major"
     title="FAC cohort #7"
-    img={bez}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“The constant focus on pairing and working together means that you're never stuck banging your head against a wall by yourself. You always have someone else to work through the problem with you. After leaving Founders and Coders, I had a lot of faith in my own ability to learn new technologies, which was crucial when I founded my startup, Fat Llama. ”"
@@ -287,7 +305,8 @@ const Ruth = () => (
     firstName="Ruth"
     secondName="Uwedenimo"
     title="FAC cohort #6"
-    img={bez}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“The best part of my experience with Founders and Coders are the people. It is a great community, with members of past cohorts teaching the current cohort. Everyone supports each other in their learning and wants to give back. ”"
@@ -298,7 +317,8 @@ const Fatimat = () => (
     firstName="Fatimat"
     secondName="Gbajabiamila"
     title="FAC cohort #12"
-    img={bez}
+    blueImg={fatimatBlue}
+    normalImg={fatimatPlain}
     cardWidth={{ l: "50vw", m: "70vw", s: "90vw" }}
     cardHeight={{ l: "80vh", m: "50vh", s: "30vh" }}
     quote="“Joe, our commercial manager, was supportive during and after Founders and Coders. Everybody's situation is different, which Joe recognizes and takes it into account when advising us. He listened to my concerns about working in the industry and paid attention to the kind of environment I wanted to work in, which helped him place me with my current employer, 27Partners.”"
