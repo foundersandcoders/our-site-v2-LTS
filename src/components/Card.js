41d822da5c99cdb56c57a9b7ff27f6c7862cb33d
Jessie Beech <jessie@jessiebeech.com>
2018-11-22 15:40:46 +0000

    added all new facster portraits to home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,6 +8,10 @@ import fatimatBlue from "../assets/portraits/fatimat_blue.png"
 import fatimatPlain from "../assets/portraits/fatimat_plain.png"
 import helenBlue from "../assets/portraits/helen_blue.png"
 import helenPlain from "../assets/portraits/helen_plain.png"
+import owenBlue from "../assets/portraits/owen_blue.png"
+import owenPlain from "../assets/portraits/owen_plain.png"
+import ruthBlue from "../assets/portraits/ruth_blue.png"
+import ruthPlain from "../assets/portraits/ruth_plain.png"
 
 const Container = styled.div.attrs({
   className:
@@ -303,17 +307,6 @@ const Rebecca = () => (
   />
 )
 
-const Bez = () => (
-  <Card
-    firstName="Besart"
-    secondName="Hoxhaj"
-    title="FAC cohort #2"
-    blueImg={fatimatBlue}
-    normalImg={fatimatPlain}
-    quote="“The most valuable thing I’ve taken away from the course is the community. 100%. On a personal level it helped me find a real sense of purpose when I needed one, brought on by the amount of people that have worked so hard and how much energy has been invested in it to keep it going.”"
-  />
-)
-
 const Amelie = () => (
   <Card
     firstName="Amelie"
@@ -340,8 +333,8 @@ const Owen = () => (
     secondName="Turner-"
     thirdName="Major"
     title="FAC cohort #7"
-    blueImg={fatimatBlue}
-    normalImg={fatimatPlain}
+    blueImg={owenBlue}
+    normalImg={owenPlain}
     quote="“The constant focus on pairing and working together means that you're never stuck banging your head against a wall by yourself. You always have someone else to work through the problem with you. After leaving Founders and Coders, I had a lot of faith in my own ability to learn new technologies, which was crucial when I founded my startup, Fat Llama. ”"
   />
 )
@@ -350,8 +343,8 @@ const Ruth = () => (
     firstName="Ruth"
     secondName="Uwedenimo"
     title="FAC cohort #6"
-    blueImg={fatimatBlue}
-    normalImg={fatimatPlain}
+    blueImg={ruthBlue}
+    normalImg={ruthPlain}
     quote="“The best part of my experience with Founders and Coders are the people. It is a great community, with members of past cohorts teaching the current cohort. Everyone supports each other in their learning and wants to give back. ”"
   />
 )
@@ -366,16 +359,4 @@ const Fatimat = () => (
   />
 )
 
-export {
-  Dan,
-  Ines,
-  Yvonne,
-  Joe,
-  Rebecca,
-  Bez,
-  Amelie,
-  Helen,
-  Owen,
-  Ruth,
-  Fatimat,
-}
+export { Dan, Ines, Yvonne, Joe, Rebecca, Amelie, Helen, Owen, Ruth, Fatimat }
