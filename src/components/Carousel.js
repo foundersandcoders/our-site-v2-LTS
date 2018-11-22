45c54150c01341ba94d1208f71f87a9e45b0ea27
Jessie Beech <jessie@jessiebeech.com>
2018-11-22 16:50:23 +0000

    carousel title in correct place

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -114,10 +114,7 @@ class Carousel extends Component {
           >
             <_InnerApplicationContainer>
               <InnerGridContainer>
-                <HeadingWithBody
-                  title={title}
-                  bgColour="light-gray pr4 pv4 pl4"
-                >
+                <HeadingWithBody title={title} bgColour="light-gray pv4">
                   <_Carousel scrollY={scrollY} carouselWidth={carouselWidth}>
                     {children}
                   </_Carousel>
