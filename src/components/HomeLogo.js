af89ba863363545ea8761c241e66b2756b7f3315
Ivan <ivangonzalez@rocketmail.com>
2018-12-25 19:31:22 +0000

    Add link home page link to fac logo

--- src/components/HomeLogo.js
+++ src/components/HomeLogo.js
@@ -1,12 +1,13 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
+
 import logoBlack from "../assets/logos/fac_logo.svg"
 import logoWhite from "../assets/logos/fac_logo_white.png"
 import { breakpoint } from "../styles/utils"
 
 const Container = styled.div`
-  width: ${({ menu }) => (menu ? "30vw" : "20%")};
+  width: ${({ styled: { menu } }) => (menu ? "30vw" : "20%")};
   max-width: 432px;
   min-width: 180px;
 `
@@ -15,16 +16,44 @@ const LogoStyled = styled.img.attrs({
   className: "db aspect-ratio mt6",
   src: ({ color }) => (color && color !== "yellow" ? logoWhite : logoBlack),
 })`
-  padding-bottom: 43.48%;
-  height: 107px;
+  padding-bottom: 43.684210526%;
+
+  ${breakpoint.ns`
+    height: calc(100vw * 0.079);
+  `};
+
   ${breakpoint.m`
     height: 78px;
   `};
+
+  height: 107px;
+  max-height: 107px;
+`
+
+const Link = styled.div.attrs({
+  className: "mt6 pointer",
+})`
+  position: absolute;
+  width: 100%;
+  background-color: transparent;
+  top: 0;
+
+  ${breakpoint.ns`
+    height: calc(100vw * 0.079);
+  `};
+
+  ${breakpoint.m`
+    height: 78px;
+  `};
+
+  height: 107px;
+  max-height: 107px;
 `
 
 const HomeLogo = ({ className, color, menu }) => (
-  <Container className="aspect-ratio--object" menu={menu}>
-    <LogoStyled className={className} color={color} to="/" />
+  <Container className="aspect-ratio--object" styled={{ menu: menu }}>
+    <LogoStyled className={className} color={color} />
+    <Link onClick={() => navigate("/")} />
   </Container>
 )
 
