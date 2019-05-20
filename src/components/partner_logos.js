1f97ca328511223324639a600446ee12e66897d6
Charlie La Fosse <charlielafosse@outlook.com>
2019-05-20 16:58:32 +0100

    comment out partner logos

--- src/components/partner_logos.js
+++ src/components/partner_logos.js
@@ -1,7 +1,7 @@
 import Light from "../assets/logos/partner_logos/8th_Light.png"
 import Partners from "../assets/logos/partner_logos/27Partners.jpg"
 import Arup from "../assets/logos/partner_logos/Arup.png"
-import Blow from "../assets/logos/partner_logos/Blow.png"
+// import Blow from "../assets/logos/partner_logos/Blow.png"
 import Byte from "../assets/logos/partner_logos/Byte_London.png"
 import CAST from "../assets/logos/partner_logos/CAST.png"
 import CTilbury from "../assets/logos/partner_logos/Tilbury.png"
@@ -18,17 +18,17 @@ import Globetown from "../assets/logos/partner_logos/Globetown.png"
 import GoSweat from "../assets/logos/partner_logos/GoSweat.png"
 import KoruKids from "../assets/logos/partner_logos/KoruKids.png"
 import MadebyMany from "../assets/logos/partner_logos/MadebyMany.png"
-import Nested from "../assets/logos/partner_logos/Nested.png"
-import Night_Zoo from "../assets/logos/partner_logos/Night_Zookeeper.png"
+// import Nested from "../assets/logos/partner_logos/Nested.png"
+// import Night_Zoo from "../assets/logos/partner_logos/Night_Zookeeper.png"
 import Power from "../assets/logos/partner_logos/Power_to_Change.jpg"
 import RDG from "../assets/logos/partner_logos/Rail_Delivery_Group.jpg"
 import Red_Sift from "../assets/logos/partner_logos/Red_Sift.png"
 import Spill from "../assets/logos/partner_logos/Spill.png"
-import Syrona from "../assets/logos/partner_logos/Syrona.png"
+// import Syrona from "../assets/logos/partner_logos/Syrona.png"
 import Thoughtworks from "../assets/logos/partner_logos/Thoughtworks.jpg"
 import Ticketmaster from "../assets/logos/partner_logos/Ticketmaster.png"
 import ZeroDeposit from "../assets/logos/partner_logos/ZeroDeposit.png"
-import Twine from "../assets/logos/partner_logos/Twine.png"
+// import Twine from "../assets/logos/partner_logos/Twine.png"
 import Unruly from "../assets/logos/partner_logos/Unruly.png"
 import verkstedt from "../assets/logos/partner_logos/verkstedt.png"
 import Wellcome from "../assets/logos/partner_logos/Wellcome_Trust.png"
@@ -36,7 +36,7 @@ import Wellcome from "../assets/logos/partner_logos/Wellcome_Trust.png"
 export default [
   Partners,
   Light,
-  Blow,
+  // Blow,
   Byte,
   Unruly,
   Ctrl,
@@ -52,19 +52,19 @@ export default [
   GoSweat,
   Globetown,
   KoruKids,
-  Night_Zoo,
-  Nested,
+  // Night_Zoo,
+  // Nested,
   verkstedt,
   Power,
   CAST,
   Red_Sift,
-  Syrona,
+  // Syrona,
   RDG,
   Spill,
   Thoughtworks,
   ZeroDeposit,
   Ticketmaster,
-  Twine,
+  // Twine,
   Arup,
   MadebyMany,
   Wellcome,
